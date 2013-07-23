f2bcca3331b17f9dc5bf049c69c352a5bcd00b2b
Richard Vogt <richvogtjr@gmail.com>
2013-07-23 07:40:02 -0500

    Tests added

--- spec/models/manuscript_spec.rb
+++ spec/models/manuscript_spec.rb
@@ -1,19 +1,22 @@
 require 'spec_helper'
 
 describe Manuscript do
-  it "can be instantiated" do
+  it "Can be instantiated" do
     Manuscript.new.should be_an_instance_of(Manuscript)
   end
 
-  it "has a valid factory" do
+  it "Has a valid factory" do
     FactoryGirl.create(:manuscript).should be_valid
   end
   
-  it "is invalid without a title" do
+  it "Is invalid without a title" do
     FactoryGirl.build(:manuscript, title: nil).should_not be_valid
   end
 
-  it "is invalid without an author" do
+  it "Is invalid without an author" do
     FactoryGirl.build(:manuscript, author: nil).should_not be_valid
   end
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:author) }
 end
--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe 'Home' do
-  context "on the home page" do
+describe 'Home Page' do
+  context 'On the home page' do
 
-    it "can edit a post by clicking the edit link next to a post" do
-      visit index
-      click_link "Edit"
+    it 'Welcomes the user' do
+      visit '/'
+      page.should have_content('How about that?')
     end
 
   end
